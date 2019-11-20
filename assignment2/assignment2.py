606af1ceafd9228876cdeca281ec8b88474b32d2
Beomsu Lim <lbs1163@postech.ac.kr>
2019-11-20 18:15:58 +0900

    Save

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -12,33 +12,17 @@ def task_1_3_apply_filters(filename1, filename2):
     color_gaussian = cv.GaussianBlur(np.asarray(color_raw), (3, 3), 0)
     depth_gaussian = cv.GaussianBlur(np.asarray(depth_raw), (3, 3), 0)
 
-    color_sobel = cv.Sobel(np.asarray(color_raw), -1, 1, 1)
+    color_sobel = cv.Sobel(np.asarray(color_raw), -1, 1, 0)
     depth_sobel = cv.Sobel(np.asarray(depth_raw), -1, 1, 1)
 
     rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
     rgbd_image_gaussian = o3d.geometry.RGBDImage.create_from_color_and_depth(o3d.geometry.Image(color_gaussian), o3d.geometry.Image(depth_gaussian))
 
     plt.subplot(1, 2, 1)
-    plt.imshow(depth_raw)
+    plt.imshow(color_raw)
     plt.subplot(1, 2, 2)
-    plt.imshow(depth_gaussian)
+    plt.imshow(color_sobel)
     plt.show()
-
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
-        rgbd_image,
-        o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
-    )
-    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-
-    o3d.visualization.draw_geometries([pcd])
-
-    pcd_gaussian = o3d.geometry.PointCloud.create_from_rgbd_image(
-        rgbd_image_gaussian,
-        o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
-    )
-    pcd_gaussian.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-    
-    o3d.visualization.draw_geometries([pcd_gaussian])
 
     return
 
