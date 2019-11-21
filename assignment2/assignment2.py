3ac6f1ad815da576c77429ba4794f594e5991eb7
lbs1163 <lbs1163@postech.ac.kr>
2019-11-21 10:36:45 +0900

    Save

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -10,24 +10,30 @@ def task_1_3_apply_filters(file_color1, file_depth1, file_color2, file_depth2):
         color_raw = o3d.io.read_image(file_color1)
         depth_raw = o3d.io.read_image(file_depth1)
 
-        color_gaussian = cv.GaussianBlur(np.asarray(color_raw), (3, 3), 0)
+        grayscale_raw = cv.cvtColor(np.asarray(color_raw), cv.COLOR_BGR2GRAY)
+        #grayscale_raw = np.asarray(color_raw)
 
-        color_sobel_dx = cv.Sobel(np.asarray(color_raw), -1, 1, 0)
-        color_sobel_dy = cv.Sobel(np.asarray(color_raw), -1, 0, 1)
+        grayscale_gaussian = cv.GaussianBlur(grayscale_raw, (3, 3), 0)
+
+        grayscale_sobel_dx = cv.Sobel(np.asarray(grayscale_raw), -1, 1, 0)
+        grayscale_sobel_dy = cv.Sobel(np.asarray(grayscale_raw), -1, 0, 1)
 
         return {
-            "color": {
-                "raw": color_raw,
-                "gaussian": color_gaussian,
+            "grayscale": {
+                "raw": grayscale_raw,
+                "gaussian": grayscale_gaussian,
                 "sobel": {
-                    "dx": color_sobel_dx,
-                    "dy": color_sobel_dy
+                    "dx": grayscale_sobel_dx,
+                    "dy": grayscale_sobel_dy
                 }
             },
             "depth": depth_raw
         }
 
     return [read(file_color1, file_depth1), read(file_color2, file_depth2)];
+
+def task_1_4_calculate_jacobian_matrix(image):
+    return
 
 if __name__ == "__main__":
     images = task_1_3_apply_filters(
@@ -36,25 +42,25 @@ if __name__ == "__main__":
     )
 
     plt.subplot(2, 3, 1)
-    plt.imshow(images[0]["color"]["raw"])
+    plt.imshow(images[0]["grayscale"]["raw"])
     plt.subplot(2, 3, 2)
-    plt.imshow(images[0]["color"]["gaussian"])
+    plt.imshow(images[0]["grayscale"]["gaussian"])
     plt.subplot(2, 3, 3)
     plt.imshow(images[0]["depth"])
     plt.subplot(2, 3, 5)
-    plt.imshow(images[0]["color"]["sobel"]["dx"])
+    plt.imshow(images[0]["grayscale"]["sobel"]["dx"])
     plt.subplot(2, 3, 6)
-    plt.imshow(images[0]["color"]["sobel"]["dy"])
+    plt.imshow(images[0]["grayscale"]["sobel"]["dy"])
     plt.show()
 
     plt.subplot(2, 3, 1)
-    plt.imshow(images[1]["color"]["raw"])
+    plt.imshow(images[1]["grayscale"]["raw"])
     plt.subplot(2, 3, 2)
-    plt.imshow(images[1]["color"]["gaussian"])
+    plt.imshow(images[1]["grayscale"]["gaussian"])
     plt.subplot(2, 3, 3)
     plt.imshow(images[1]["depth"])
     plt.subplot(2, 3, 5)
-    plt.imshow(images[1]["color"]["sobel"]["dx"])
+    plt.imshow(images[1]["grayscale"]["sobel"]["dx"])
     plt.subplot(2, 3, 6)
-    plt.imshow(images[1]["color"]["sobel"]["dy"])
+    plt.imshow(images[1]["grayscale"]["sobel"]["dy"])
     plt.show()
