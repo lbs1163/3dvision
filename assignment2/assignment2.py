0026fa3927edb00d9f02521ecda41b2556ba6d08
lbs1163 <lbs1163@postech.ac.kr>
2019-11-23 21:00:30 +0900

    Visualize

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -23,6 +23,7 @@ def task_1_3_apply_filters(file_color1, file_depth1, file_color2, file_depth2):
         grayscale_sobel_dy = cv.Sobel(grayscale_gaussian, -1, 0, 1)
 
         return {
+            "color_raw": color_raw,
             "grayscale": {
                 "raw": grayscale_raw,
                 "gaussian": grayscale_gaussian,
@@ -31,35 +32,116 @@ def task_1_3_apply_filters(file_color1, file_depth1, file_color2, file_depth2):
                     "dy": grayscale_sobel_dy
                 }
             },
-            "depth": depth_raw
+            "depth": np.asarray(depth_raw),
+            "depth_raw": depth_raw,
         }
 
     return {"i": read(file_color1, file_depth1), "j": read(file_color2, file_depth2)};
 
-def task_1_4_calculate_jacobian_matrix(image):
+def task_1_4_calculate_jacobian_matrix(image, T):
+    I_j = image["grayscale"]["gaussian"][image["depth"] != 0]
+    size = I_j.size
+
     [v, u] = np.indices(image["grayscale"]["gaussian"].shape)
-    d = np.reshape(image["depth"], (image["depth"].size, 1))
-    h_x = False
+    u = np.reshape(u[image["depth"] != 0], size)
+    v = np.reshape(v[image["depth"] != 0], size)
+    d = np.reshape(image["depth"][image["depth"] != 0], size)
 
-    dx = np.reshape(image["grayscale"]["sobel"]["dx"], (image["grayscale"]["sobel"]["dx"].size, 1))
-    dy = np.reshape(image["grayscale"]["sobel"]["dy"], (image["grayscale"]["sobel"]["dy"].size, 1))
+    h_x = (u - c_x) * d / f_x
+    h_y = (v - c_y) * d / f_y
+    h_z = d
+    h = np.stack((h_x, h_y, h_z, np.ones(size)), axis=0)
 
-    I_g = np.concatenate((dx, dy), 1)
+    s = T @ h
+    s_x = s[0] / s[3]
+    s_y = s[1] / s[3]
+    s_z = s[2] / s[3]
 
-    g_s = np.array([
-        [f_x/s_z,         0, - (f_x * s_x) / (s_z ** 2)],
-        [      0, f_y / s_z, - (f_y * s_y) / (s_z ** 2)],
-    ])
+    g_u = s_x * f_x / s_z + c_x
+    g_v = s_y * f_y / s_z + c_y
 
-    s_xi = np.array([
-        [   0,  h_z, -h_y, 1, 0, 0],
-        [-h_z,    0,  h_z, 0, 1, 0],
-        [ h_y, -h_x,    0, 0, 0, 1],
-    ])
+    dx = np.reshape(image["grayscale"]["sobel"]["dx"][image["depth"] != 0], (size, 1))
+    dy = np.reshape(image["grayscale"]["sobel"]["dy"][image["depth"] != 0], (size, 1))
 
-    J_r = np.matmul(I_g, np.matmul(g_s, s_xi))
+    J_r = np.zeros((size, 6))
 
-    return J_r
+    for i in range(0, size):
+        # 1 x 2
+        I_g = np.transpose(np.array([dx[i], dy[i]]))
+
+        # 2 x 3
+        g_s = np.array([
+            [f_x/s_z[i],            0, - (f_x * s_x[i]) / (s_z[i] ** 2)],
+            [         0, f_y / s_z[i], - (f_y * s_y[i]) / (s_z[i] ** 2)],
+        ])
+        
+        # 3 x 6
+        s_xi = np.array([
+            [      0,  h_z[i], -h_y[i], 1, 0, 0],
+            [-h_z[i],       0,  h_z[i], 0, 1, 0],
+            [ h_y[i], -h_x[i],       0, 0, 0, 1],
+        ])
+
+        J_r[i] = I_g @ g_s @ s_xi
+    
+    return (J_r, I_j, g_u, g_v)
+
+def task_1_5_find_transformation_matrix(images):
+    xi = np.array([0, 0, 0, 0, 0, 0])
+
+    source_rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(images["j"]["color_raw"], images["j"]["depth_raw"])
+    target_rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(images["j"]["color_raw"], images["j"]["depth_raw"])
+
+    source = o3d.geometry.PointCloud.create_from_rgbd_image(
+        source_rgbd, o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    )
+    target = o3d.geometry.PointCloud.create_from_rgbd_image(
+        target_rgbd, o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    )
+
+    source.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    target.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(source)
+    vis.add_geometry(target)
+
+    T = np.identity(4)
+
+    for i in range(0, 10):
+        J_r, I_j, u, v = task_1_4_calculate_jacobian_matrix(images["j"], T)
+
+        r = np.zeros(I_j.size)
+
+        for j in range(0, I_j.size):
+            x = round(u[i])
+            y = round(v[i])
+
+            [h, w] = images["i"]["grayscale"]["gaussian"].shape
+
+            if x < 0 or x >= w or y > 0 or y >= h:
+                I_i = 0
+            else:
+                I_i = images["i"][y][x]
+            
+            r[i] = I_i - I_j[j]
+
+        xi = xi - np.linalg.inv(np.transpose(J_r) @ J_r) @ np.transpose(J_r) @ r
+
+        T = np.array([
+            [     1, -xi[2],  xi[1], xi[3]],
+            [ xi[2],      1, -xi[0], xi[4]],
+            [-xi[1],  xi[0],      1, xi[5]],
+            [     0,      0,      0,     1],
+        ])
+
+        source.transform(T)
+        vis.update_geometry()
+        vis.poll_events()
+        vis.update_renderer()
+
+    return T
 
 if __name__ == "__main__":
     show_results = False
@@ -69,9 +151,7 @@ if __name__ == "__main__":
         "./data/image00894.png", "./data/depth00894.png"
     )
 
-    jacobian_matrix = task_1_4_calculate_jacobian_matrix(images["i"])
-
-    print(jacobian_matrix.shape)
+    T = task_1_5_find_transformation_matrix(images)
 
     if show_results:
         plt.subplot(2, 3, 1)
